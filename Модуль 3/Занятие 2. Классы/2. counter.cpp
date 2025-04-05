f9f0d528fa9b3012243d967182c90e9b7a8a4876
Andrey Shepelov <photo.buzuluk@mail.ru>
2025-04-05 22:08:18 +0500

    change counter.cpp

--- Модуль 3/Занятие 2. Классы/2. counter.cpp
+++ Модуль 3/Занятие 2. Классы/2. counter.cpp
@@ -1,27 +1,29 @@
 ﻿#include <iostream>
 #include <clocale>
 
-class Counter{
+class Counter {
 private:
 	int value;
 public:
-	Counter(){
+	Counter() {
 		value = 1;
 	}
 
-	Counter(int init){
+	Counter(int init) {
 		value = init;
 	}
 
-	int command(char c){
-		switch (c){
-		case '+':
-			value++;
-			break;
-		case '-':
-			value--;
-			break;
-		}
+	int increase() {
+		value++;
+		return value;
+	}
+
+	int decrease() {
+		value--;
+		return value;
+	}
+
+	int showValue() {
 		return value;
 	}
 };
@@ -37,32 +39,36 @@ int main(int argc, char argv[])
 
 	std::cout << "Вы хотите указать начальное значение счетчика? Введите \"Y\" или \"N\": ";
 	std::cin >> start_initial;
-	if (start_initial == 'Y'){
+	if (start_initial == 'Y') {
 		std::cout << "Введите начальное значение счетчика (целое число): ";
 		std::cin >> init;
 		cnt = new Counter(init);
 	}
-	else{
+	else {
 		cnt = new Counter();
 	}
 
-	do{
+	do {
 		std::cout << "Введите команду (\"+\", \"-\", \"=\" или \"x\"): ";
 		std::cin >> command;
-		switch (command){
+		switch (command) {
 		case 'x':
 			break;
 		case '+':
+			std::cout << "Значение счетчика: " << cnt->increase() << std::endl << std::endl;
+			break;
 		case'-':
+			std::cout << "Значение счетчика: " << cnt->decrease() << std::endl << std::endl;
+			break;
 		case '=':
-			std::cout << "Значение счетчика: " << cnt->command(command) << std::endl << std::endl;
+			std::cout << "Значение счетчика: " << cnt->showValue() << std::endl << std::endl;
 			break;
 		default:
 			std::cout << "Вы ввели некорректную команду!" << std::endl << std::endl;
 			break;
 		}
-	}
-	while (command != 'x');
+	} while (command != 'x');
+	delete cnt;
 	std::cout << "Работа программы завершена! До свидания!" << std::endl;
 
 	return 0;
